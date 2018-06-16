d05cb8f2c264c80f37e623f3d5d380b855937151
takamisris <takamisris@gmail.com>
2018-06-16 19:34:32 +0900

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const multer = require('multer')
 const fs = require('fs')
 const app = express()
-const IMAGE_PATH = '/var/image'
+const IMAGE_DIR = '/var/image'
 // const IMAGE_DIR = '/Users/amisris/tmp'
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -21,12 +21,12 @@ const server = app.listen(3000)
 // app.use(express.static(IMAGE_DIR))
 
 app.get('/:image', function(req, res, next) {
-  console.log(req.params.image)
+  console.log('requested: ' + IMAGE_DIR + '/' + req.params.image)
   res.header("Access-Control-Allow-Origin", "*");
   res.sendFile(IMAGE_DIR + '/' + req.params.image)
 })
 
-app.post('/image', upload.single('file'), function(req, res, next) {
+app.post('/image', upload.single('file'), async function(req, res, next) {
   const req_file_json = JSON.stringify(req.file)
   const req_file = JSON.parse(req_file_json)
   const image_path = req_file.path
@@ -35,19 +35,34 @@ app.post('/image', upload.single('file'), function(req, res, next) {
   const req_body = JSON.parse(req_body_json)
   const label = req_body.picked
 
-  // when python script is "do_generate.py" which takes 2 args, it seems like
-  // ----------------------------------
-  // do_generate.py image_path label
-  // ----------------------------------
-  // image_path : posted file path which might be /var/images/[file_name]
-  // label : correct label the uploaded file has
-  
-  // console.log(req_file)
-  console.log(IMAGE_DIR + '/' + image_path)
-  
+  console.log('saved: ' + image_path)
+
+  let result = ''
+  await exec_generate_image(image_path, label)
+  .then(function(res) {
+    console.log(res)
+    result = 'success'
+  })
+  .catch(function(err) {
+    result = 'failed'
+  })
+
   // const buf = fs.readFileSync(IMAGE_DIR + '/' + image_path);
   res.header("Access-Control-Allow-Origin", "*");
   // res.send(buf, { 'Content-Type': 'image/png' }, 200);
 
-  res.json({'result': 'success!'})
+  res.json({'result': `${result}`})
 })
+
+function exec_generate_image(image_path, label) {
+  return new Promise(function(resolve, reject) {
+    const exec = require('child_process').exec
+    const COMMAND = `python generate.py ${image_path} ${label}`
+    exec(COMMAND, function(error, stdout, stderr) {
+      if (error) {
+        reject(error)
+      }
+      resolve('generating is finished')
+    })
+  })
+}
